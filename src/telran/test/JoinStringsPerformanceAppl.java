e37760c5990157f4b58ec67d9a7a7242e79d8e3c
AlGuron <blizgur8@gmail.com>
2024-01-16 01:17:45 +0200

    add: class name constructor

--- src/telran/test/JoinStringsPerformanceAppl.java
+++ src/telran/test/JoinStringsPerformanceAppl.java
@@ -1,35 +1,41 @@
 package telran.test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 import telran.performance.JoinStringPerformanceTest;
+import telran.text.JoinStrings;
 import telran.text.JoinStringsOnBuilder;
 import telran.text.JoinStringsOnStandard;
 import telran.text.JoinStringsOnString;
 
 public class JoinStringsPerformanceAppl {
 	private static final int N_STRINGS = 1000;
+	private static final int N_RUNS = 1000;
 	private static final String[] bigArray;
+	private static final String BASE_PACKAGE = "telran.test.";
 
 	static {
 		bigArray = new String[N_STRINGS];
 		Arrays.fill(bigArray, "Hello");
 	}
 
-	public static void main(String[] args) {
-		JoinStringsOnBuilder joinBuilder = new JoinStringsOnBuilder();
-		JoinStringsOnString joinString = new JoinStringsOnString();
-		JoinStringsOnStandard joinStandartString = new JoinStringsOnStandard();
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			System.out.println("Usage <name of class as an argument>");
+		} else {
+			Class<JoinStrings> classJS = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + args[0]);
+			Constructor<JoinStrings> constructor = classJS.getConstructor();
+			JoinStrings instance = constructor.newInstance();
+			JoinStringPerformanceTest test = new JoinStringPerformanceTest(getTestName(args[0]), N_RUNS, bigArray,
+					instance);
+			test.run();
+		}
+	}
 
-		JoinStringPerformanceTest BuilderTest = new JoinStringPerformanceTest("JoinBuilderTest", 10000, bigArray,
-				joinBuilder);
-		JoinStringPerformanceTest StringTest = new JoinStringPerformanceTest("JoinStringTest", 1000, bigArray,
-				joinString);
-		JoinStringPerformanceTest StandartStringTest = new JoinStringPerformanceTest("JoinStandartStringTest", 10000,
-				bigArray, joinStandartString);
-
-		BuilderTest.run();
-		StringTest.run();
-		StandartStringTest.run();
+	private static String getTestName(String className) {
+		return String.format("%s; Number of the strings is %d", className, N_STRINGS);
 	}
 }
