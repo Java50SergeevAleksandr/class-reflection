94e6142ca092d0fad8df1fb18a70be7fa8e44276
AlGuron <blizgur8@gmail.com>
2024-01-15 23:50:27 +0200

    init

--- src/telran/reflect/Footballer.java
+++ src/telran/reflect/Footballer.java
@@ -1,6 +1,16 @@
 package telran.reflect;
 
 public class Footballer implements Sportsman {
+	private static final String DEFAULT_TEAM = "Real Madrid";
+	String team;
+
+	Footballer(String team) {
+		this.team = team;
+	}
+
+	Footballer() {
+		this(DEFAULT_TEAM);
+	}
 
 	@Override
 	public void action() {
--- src/telran/reflect/Runner.java
+++ src/telran/reflect/Runner.java
@@ -1,6 +1,16 @@
 package telran.reflect;
 
 public class Runner implements Sportsman {
+	private static final String DEFAULT_DISTANCE = "1200m";
+	String distance;
+
+	Runner(String distance) {
+		this.distance = distance;
+	}
+
+	Runner() {
+		this(DEFAULT_DISTANCE);
+	}
 
 	@Override
 	public void action() {
